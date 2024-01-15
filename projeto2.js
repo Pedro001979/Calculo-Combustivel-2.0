0c8d02cb8c852db421f3a658f1f628443e32d7ac
Pedro001979 <144935572+Pedro001979@users.noreply.github.com>
2024-01-15 18:10:50 -0300

    Update projeto2.js

--- projeto2.js
+++ projeto2.js
@@ -1,11 +1,3 @@
-/* Este é um Programa simples em Java Script que calcula o valor de uma viagem
-tendo 5 variaveis. Sendo elas:
-1. Preço da Gasolina;
-2. Preço do Etanol;
-3. O  tipo de combustivel que está no carro;
-4. Gasto Médio do Combustivel;
-5. Distancia Percorrida em Km da viagem;
-*/
 const precoEtanol = 3.89;
 const precoGasolina = 5.56;
 const kmPorLitros = 10;
